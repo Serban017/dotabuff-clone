137cfd8275d54757d4b3cfcf907d3945bf2c5e75
Serban <serbanargetoianu@gmail.com>
2026-03-25 23:25:30 +0200

    added hero data caching

--- heroes.py
+++ heroes.py
@@ -1,19 +1,27 @@
 import requests
+import os
+import json
 from pprint import pprint
+
+
 
 
 def get_hero_name():
     
-    response = requests.get("https://api.opendota.com/api/heroes")
+    if os.path.exists("heroes_data.json"):
+        with open("heroes_data.json", "r") as f:
+            heroes_data = json.load(f)
+    else:
+        response = requests.get("https://api.opendota.com/api/heroes")
+        heroes_data = response.json()
+        with open("heroes_data.json", "w") as f:
+            json.dump(heroes_data, f)
 
-    heroes_data = response.json()
 
     hero_table = {}
 
     for hero in heroes_data:
         hero_table[hero['id']] = hero['localized_name']
         
-        
     return hero_table
 
-get_hero_name()
