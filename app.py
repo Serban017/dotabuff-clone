8499451c3d740727e6ab22168a6bae8d7ce82bf7
Serban <serbanargetoianu@gmail.com>
2026-03-28 02:58:40 +0200

    added flask web interface

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests
 from heroes import get_hero_name
  
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+
 
 app = Flask(__name__)
 
@@ -9,14 +10,26 @@ hero_table = get_hero_name()
 
 @app.route('/')
 def home():
-    return "Welcome to Dotabuff Clone!"
+    return render_template('home.html')
+
+@app.route('/player')
+def player_search():
+    player_id = request.args.get('player_id')
+    return redirect(f'/player/{player_id}')
 
 @app.route('/player/<player_id>')
 def player(player_id):
     response = requests.get(f"https://api.opendota.com/api/players/{player_id}/recentMatches")
     recent_matches = response.json()
-    first_match = recent_matches[0]
-    return f"Match {first_match['match_id']} - WIN - Hero: {hero_table[first_match['hero_id']]} - KDA: {first_match['kills']}/{first_match['deaths']}/{first_match['assists']}"
+   
+    for match in recent_matches:
+        if match['player_slot'] <= 4 and match['radiant_win'] == True:
+            match['result'] = 'WIN'
+        elif match['player_slot'] >= 128 and match['radiant_win'] == False:
+            match['result'] = 'WIN'
+        else:
+            match['result'] = 'LOSS'
+    return render_template('player.html', matches=recent_matches, hero_table=hero_table)
 
 if __name__ == '__main__':
     app.run(debug=True)
